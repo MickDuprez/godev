b7272f1108731e43fa1adc4467f12b9039fdd45c
Mick Duprez <mick.duprez@outlook.com>
2018-11-10 17:05:08 +1100

    first commit

---
+++ FizzBuzzGo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// FizzBuzzGo:
+// An implementation of the Fizz Buzz problem and described at
+// The Rosetta Code:> https://rosettacode.org/wiki/FizzBuzz
+// Purpose:
+// This application is an exercise to get more familiar with the Go
+// language and tooling such as Testing, Benchmarking and Documentation.
+func main() {
+	fmt.Println("Hello, World!")
+}
